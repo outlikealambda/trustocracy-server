ba0a710d6fa09232e93ebc7caf828ac2eefe9686
Will Reppun <wreppun@outsideiq.com>
2016-01-11 14:40:00 -1000

    serve wrapped api with express

---
+++ app.js
@@ -0,0 +1,44 @@
+var
+  express = require('express'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+
+  db = require('./graph');
+
+
+var app = express();
+
+// Configuration
+
+app.set('port', process.env.PORT || 3714);
+
+// from 2.0 example
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get('/api/user/:id', function(req, res) {
+  res.set({ 'Content-Type': 'application/json' });
+
+  db.getUserInfo(req.params.id).then(userInfo => {
+    res.send(userInfo);
+  });
+});
+
+app.get('/api/user/:userId/topic/:topicId/opinions', function(req, res) {
+  var {userId, topicId} = req.params;
+
+  res.set({ 'Content-Type': 'application/json' });
+
+  db.getNearestOpinions(userId, topicId).then(nearestOpinions => {
+    res.send(nearestOpinions);
+  });
+});
+
+
+// Start server
+app.listen(app.get('port'), function() {
+  console.log('Starting node');
+});
