f8933e0b8be1b0999e91dd4795891a02b1f1ab58
Will Reppun <reppun@gmail.com>
2016-04-15 14:54:57 -1000

    retrieve google certs and respect cache-control settings

--- googleAuth.js
+++ googleAuth.js
@@ -1,31 +1,80 @@
 'use strict';
 
 const
-  jwt = require('jsonwebtoken');
+  jwt = require('jsonwebtoken'),
+  rp = require('request-promise'),
+  _ = require('lodash'),
+  certFetchOptions = {
+    method: 'GET',
+    uri: 'https://www.googleapis.com/oauth2/v1/certs',
+    resolveWithFullResponse: true
+  };
+
+
+let
+  certificateExpiry = null,
+  certificateCache = {};
+
 
 function asyncValidate(token, certs, callback) {
+  getCerts().then(certObj => {
 
-  jwt.verify(
-    token,
-    certs['bdb7149ce3018a17a225db0abc4939bea50573b2'],
-    { algorithms: ['RS256'] },
-    tryAgainIfWrongKey
-  );
+    let
+      error = null,
+      certs = _.values(certObj);
 
-  function tryAgainIfWrongKey(err, payload) {
-    if (!err) {
-      callback(err, payload);
-      return;
+    for (let i = 0; i < certs.length; i++) {
+      try {
+        let payload = jwt.verify(
+          token,
+          certs[i],
+          { algorithms: ['RS256'] }
+        );
+
+        // found a valid cert
+        callback(null, payload);
+        return;
+
+      } catch (err) {
+        error = err;
+      }
+
     }
 
-    jwt.verify(
-      token,
-      certs['4cb1c44619bc972cb746d6095fe82afcce4e62e9'],
-      { algorithms: ['RS256'] },
-      callback
-    );
-  }
+    // no valid certs found
+    callback(error, null);
+  });
 }
+
+
+function getCerts() {
+  if (certificateExpiry && Date.now() < certificateExpiry.getTime()) {
+    return certificateCache;
+  }
+
+  return rp(certFetchOptions).then(res => {
+    const
+      cacheControl = res.headers['cache-control'],
+      cacheAge = !cacheControl ? -1 : extractAge(cacheControl),
+      body = JSON.parse(res.body);
+
+    // set cache expiry, and save certs for future requests
+    certificateExpiry = cacheAge === -1 ? null : new Date(Date.now() + cacheAge);
+    certificateCache = body;
+
+    return body;
+  });
+}
+
+
+function extractAge(cacheControl) {
+  const
+    pattern = new RegExp('max-age=([0-9]*)'),
+    matches = pattern.exec(cacheControl);
+
+  return matches.length === 2 ? matches[1] * 1000 : -1;
+}
+
 
 module.exports = {
   asyncValidate
