54aa0e2f4cb8df6053cdad6078f77edf9e241dee
Will Reppun <reppun@gmail.com>
2016-08-21 17:53:41 -1000

    add queries for opinion consensus

--- db/relational/query.js
+++ db/relational/query.js
@@ -38,5 +38,38 @@ module.exports = {
         AND answer.opinion_id = $<opinionId>
         AND answer.user_id = $<userId>
       `
+  },
+
+  consensus : {
+
+    topic : {
+      picked :
+        `SELECT opinion_id, question_id, picked, count(picked)
+          FROM answer
+          WHERE topic_id = $<topicId> AND picked IS NOT NULL
+          GROUP BY opinion_id, question_id, picked`,
+
+      rated :
+        `SELECT opinion_id, question_id, avg(rated)
+          FROM answer
+          WHERE topic_id = $<topicId> AND rated IS NOT NULL
+          GROUP BY opinion_id, question_id
+        `
+    },
+
+    opinion : {
+      picked :
+        `SELECT question_id, picked, count(picked)
+          FROM answer
+          WHERE opinion_id = $<opinionId> AND picked IS NOT NULL
+          GROUP BY question_id, picked`,
+
+      rated :
+        `SELECT question_id, avg(rated)
+          FROM answer
+          WHERE opinion_id = $<opinionId> AND rated IS NOT NULL
+          GROUP BY question_id
+        `
+    }
   }
 };
